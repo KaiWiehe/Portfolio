eba71a553acf1588358a4bc8335c525dbfb3fdd7
KaiWiehe <Kai_Wiehe@live.de>
2023-01-03 08:24:05 +0100

    add header, aboveTheFold and aboutMe section. Font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboveTheFoldComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
